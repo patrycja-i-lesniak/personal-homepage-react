7fd4942eba3628eeb6d0bbcabd361352d178a532
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 19:36:37 +0200

    add media queries and themes to GlobalStyle

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -13,9 +13,13 @@ html  {
     font-family: "Inter", sans-serif;
     margin: 0;
     font-size: 18px;
-    background: ${({ theme }) => theme.colors.whiteLilac};
-    color: ${({ theme }) => theme.colors.slateGray};
+    background: ${({ theme }) => theme.colors.site.background};
+    color: ${({ theme }) => theme.colors.site.text};
     letter-spacing: 0.05em;
     word-break: break-word;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+      padding-bottom: 32px;
+    }
   }
 `;
