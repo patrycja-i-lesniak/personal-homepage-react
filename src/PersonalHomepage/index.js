fb340d99809d86b67cbeb07ee982ca083e3f90b7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 18:24:58 +0200

    add Skills component to PersonalHomepage

--- src/PersonalHomepage/index.js
+++ src/PersonalHomepage/index.js
@@ -1,8 +1,20 @@
-import {Container} from "./styled";
-import {MainInformation} from "./MainInformation";
+import { Container } from "./styled";
+import { MainInformation } from "./MainInformation";
+import { Skills } from "./Skills";
+import { skills, nextSkills } from "./Skills/skills";
 
 export const PersonalHomepage = () => (
     <Container>
-        <MainInformation/>
+        <MainInformation />
+        <main>
+            <Skills
+                title="My skillset includes:"
+                skills={skills}
+            />
+            <Skills
+                title="What I want to learn next:"
+                skills={nextSkills}
+            />
+        </main>
     </Container>
 )
