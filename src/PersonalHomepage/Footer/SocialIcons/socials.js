a1c767fd6e15cb087f655f43fae981c3aa836d1c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 18:49:28 +0200

    add socialmedia to Footer and style the elemnts

---
+++ src/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -0,0 +1,28 @@
+import {styleIcon} from "./styled";
+import {ReactComponent as GithubIcon} from "./icons/github.svg";
+import {ReactComponent as FacebookIcon} from "./icons/facebook.svg";
+import {ReactComponent as LinkedInIcon} from "./icons/linkedin.svg";
+import {ReactComponent as InstagramIcon} from "./icons/instagram.svg";
+
+export const socials = [
+    {
+        name: "GitHub",
+        url: "https://github.com/patrycja-i-lesniak",
+        Icon: styleIcon(GithubIcon),
+    },
+    {
+        name: "Facebook",
+        url: "http://facebook.com",
+        Icon: styleIcon(FacebookIcon),
+    },
+    {
+        name: "LinkedIn",
+        url: "http://linkedin.com",
+        Icon: styleIcon(LinkedInIcon),
+    },
+    {
+        name: "Instagram",
+        url: "http://instagram.com",
+        Icon: styleIcon(InstagramIcon),
+    }
+]
