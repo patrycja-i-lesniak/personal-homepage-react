be8b2edd312839b78e224d90b1338029e9ebb5bc
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 11:35:55 +0200

    define primary colors used in the project

---
+++ src/Theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+    colors:{
+        scienceBlue: "#0366D6",
+        blumine: "#1F5189",
+        dodgerBlue: "#2188FF",
+        shipCove: "#6D93BE",
+        anakiwa: "#8CC2FF",
+        tropicalBlue: "#CDE0F7",
+        ironTransparent: "rgba(209, 213, 218, 0.3)",
+        white: "#FFFFFF",
+        whiteLilac: "#FBFBFE",
+        porcelain: "#F2F3F4",
+        mercury: "#e5e5e5",
+        slateGray: "#6E7E91",
+        doveGray: "#6D6D6D",
+        tundora: "#414141",
+        mineShaft: "#252525",
+        mineShaftLighter: "#313131",
+    }
+};
