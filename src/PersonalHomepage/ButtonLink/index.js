103907e69544698c84712d3545b26194a3348870
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 19:37:20 +0200

    add media queries, hover and themes to ButtonLink

--- src/PersonalHomepage/ButtonLink/index.js
+++ src/PersonalHomepage/ButtonLink/index.js
@@ -5,9 +5,17 @@ export const ButtonLink = styled.a`
     padding: 12px 16px;
     font-weight: 600;
     font-size: 20px;
-    border: 1px solid ${({ theme }) => theme.colors.ironTransparent};
-    color: ${({ theme }) => theme.colors.white};
+    border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
+    color: ${({ theme }) => theme.colors.buttonLink.text};
     border-radius: ${({ theme }) => theme.borderRadiusSmall};
-    background: ${({ theme }) => theme.colors.scienceBlue};
+    background: ${({ theme }) => theme.colors.primary};
     transition: box-shadow 0.3s;
+
+    &:hover {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.buttonLink.shadow}
+}
+
+    @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+        font-size: 18px;
+}
 `;
