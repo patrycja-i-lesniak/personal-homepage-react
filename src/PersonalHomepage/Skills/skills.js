a124287a908c949d4e657cd35aeec1f9148b3935
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 18:17:49 +0200

    add current and present skills

---
+++ src/PersonalHomepage/Skills/skills.js
@@ -0,0 +1,34 @@
+export const skills = [
+    "Semantic & accessible HTML",
+    "Error handling",
+    "Git",
+    "Responsive Web Design",
+    "React",
+    "GitHub Pull Request & Review",
+    // eslint-disable-next-line no-script-url
+    "JavaScript: ES6",
+    "React Hooks",
+    "NPM",
+    "Working with API (fetch, axios)",
+    "Immutability",
+    "Promises, Async/Await",
+    "Redux-Saga",
+    "Markdown",
+    "Debugging",
+    "React Router",
+    "Scrum",
+    "CSS Flexbox",
+    "CSS Grid",
+    "Trello",
+    "Teamwork",
+    "CSS BEM convention",
+];
+
+export const nextSkills = [
+    "JS classes",
+    "Unit testing",
+    "Node.js",
+    "TypeScript",
+    "Cypress",
+    "React Context",
+];
