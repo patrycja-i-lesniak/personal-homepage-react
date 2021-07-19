18689db0e7a3c83106f3e757c282267fecabac48
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 19:30:25 +0200

    add commons and themeLight and themeDark to theme

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,5 +1,5 @@
-export const theme = {
-    colors:{
+const colorNames = {
+    colors: {
         scienceBlue: "#0366D6",
         blumine: "#1F5189",
         dodgerBlue: "#2188FF",
@@ -19,7 +19,70 @@ export const theme = {
     }
 };
 
-export const common = {
+const common = {
+    breakpoints: {
+        mobileMax: 769,
+        tabletVerticlaMax: 991,
+        tabletHoriontalMax: 1199,
+    },
     boxShadow: "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
     borderRadiusSmall: "4px",
+};
+
+export const themeLight = {
+    ...common,
+    colors: {
+        primary: colorNames.scienceBlue,
+        textPrimary: colorNames.mineShaft,
+        site: {
+            background: colorNames.whiteLilac,
+            text: colorNames.slateGray,
+        },
+        buttonLink: {
+            text: colorNames.white,
+            border: colorNames.ironTransparent,
+            shadow: colorNames.anakiwa,
+        },
+        boxBackground: colorNames.white,
+        headerLine: colorNames.mercury,
+        tile: {
+            border: colorNames.porcelain,
+            borderHover: colorNames.tropicalBlue,
+            header: colorNames.scienceBlue,
+        },
+        themeSwitch: {
+            background: colorNames.mercury,
+            icon: colorNames.white,
+        },
+    },
+
+};
+
+export const themeDark = {
+    ...common,
+    colors: {
+        primary: colorNames.dodgerBlue,
+        textPrimary: colorNames.white,
+        site: {
+            background: colorNames.mineShaft,
+            text: colorNames.white,
+        },
+        buttonLink: {
+            text: colorNames.white,
+            border: colorNames.ironTransparent,
+            shadow: colorNames.shipCove,
+        },
+        boxBackground: colorNames.mineShaftLighter,
+        headerLine: colorNames.tundora,
+        tile: {
+            border: colorNames.tundora,
+            borderHover: colorNames.blumine,
+            header: colorNames.white,
+        },
+        themeSwitch: {
+            background: colorNames.doveGray,
+            icon: colorNames.mineShaft,
+        },
+    },
+
 };
