0bbccd5adf0d038184f354d320fc0f633ec58cbc
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-19 16:05:15 +0200

    add global styles for homepage

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -13,5 +13,9 @@ html  {
     font-family: "Inter", sans-serif;
     margin: 0;
     font-size: 18px;
+    background: ${({ theme }) => theme.colors.whiteLilac};
+    color: ${({ theme }) => theme.colors.slateGray};
+    letter-spacing: 0.05em;
+    word-break: break-word;
   }
 `;
