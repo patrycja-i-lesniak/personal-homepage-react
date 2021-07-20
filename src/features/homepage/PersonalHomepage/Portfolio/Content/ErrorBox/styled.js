371b912c4fcd8ff6eb624d57046426ec829fe7b5
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-20 06:42:23 +0200

    format code

--- src/features/homepage/PersonalHomepage/Portfolio/Content/ErrorBox/styled.js
+++ src/features/homepage/PersonalHomepage/Portfolio/Content/ErrorBox/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.article`
-margin-top:96px;
-display: grid;
-justify-items: center;
-grid-gap: 32px;
-text-align: center;
-color: ${({ theme }) => theme.colors.textPrimary};
+    margin-top:96px;
+    display: grid;
+    justify-items: center;
+    grid-gap: 32px;
+    text-align: center;
+    color: ${({ theme }) => theme.colors.textPrimary};
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         grid-gap: 24px;
@@ -14,8 +14,8 @@ color: ${({ theme }) => theme.colors.textPrimary};
 `;
 
 export const Header = styled.header`
-font-weight: 700;
-font-size: 24px;
+    font-weight: 700;
+    font-size: 24px;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         grid-gap: 17px;
