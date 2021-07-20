0d54f008ecb5df0497d64567f3d6eab9f72cb664
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-20 05:20:31 +0200

    create and style Loading component

---
+++ src/features/homepage/PersonalHomepage/Portfolio/Content/Loading/styled.js
@@ -0,0 +1,32 @@
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as Spinner } from "./spinner.svg";
+
+const rotate = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+export const Wrapper = styled.div`
+text-align: center;
+margin-top: 88px;
+font-size: 20px;
+color: ${({theme}) => theme.colors.textPrimary};
+
+@media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    font-size: 17px;
+    margin-top: 32px;
+}
+`;
+
+export const StyledSpinner = styled(Spinner)`
+    margin-top: 48px;
+    animation: ${rotate} 1s linear infinite;
+    color: ${({theme}) => theme.colors.primary};
+    height: auto;
+
+    @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+        margin-top: 32px;
+        width: 60px;
+    }
+`;
