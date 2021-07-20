946147f5759ede6d32c203743f77c4def69d1370
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-20 05:17:08 +0200

    add breakpoints and theme colors

---
+++ src/features/homepage/PersonalHomepage/Skills/styled.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import { SubHeader } from "../SubHeader";
+import { ReactComponent as BulletIcon } from "./bullet.svg";
+
+export const Section = styled.section`
+    background: ${({ theme }) => theme.colors.white};
+    padding: 32px;
+    margin-top: 72px;
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    border-radius: ${({ theme }) => theme.borderRadiusSmall};
+    transition: background 0.3s;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 48px;
+        padding: 16px;
+    }
+`;
+
+export const List = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 8px 32px;
+    list-style: none;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+        font-size: 16px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 14px;
+        grid-template-columns: 1fr;
+        margin-top: 12px;
+    }
+`;
+
+export const Item = styled.li`
+    display: flex;
+    line-height: 1.4;
+    align-items: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        line-height: 1.2;
+    }
+`;
+
+
+export const Bullet = styled(BulletIcon)`
+    color: ${({theme}) => theme.colors.primary};
+    margin-right: 16px;
+    height: auto;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        width: 6px;
+        margin-right: 8px;
+        height: 8px;
+    }
+`;
+
+export const StyledHeader = styled(SubHeader)`
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${({theme}) => theme.colors.headerLine};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        padding-bottom: 12px;
+    }
+`;
