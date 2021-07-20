5fdb82dc73236334830159f68578852e4dff9c22
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-20 07:41:25 +0200

    deleted unnecesary object

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,22 +1,20 @@
 const colorNames = {
-    colors: {
-        scienceBlue: "#0366D6",
-        blumine: "#1F5189",
-        dodgerBlue: "#2188FF",
-        shipCove: "#6D93BE",
-        anakiwa: "#8CC2FF",
-        tropicalBlue: "#CDE0F7",
-        ironTransparent: "rgba(209, 213, 218, 0.3)",
-        white: "#FFFFFF",
-        whiteLilac: "#FBFBFE",
-        porcelain: "#F2F3F4",
-        mercury: "#e5e5e5",
-        slateGray: "#6E7E91",
-        doveGray: "#6D6D6D",
-        tundora: "#414141",
-        mineShaft: "#252525",
-        mineShaftLighter: "#313131",
-    }
+    scienceBlue: "#0366D6",
+    blumine: "#1F5189",
+    dodgerBlue: "#2188FF",
+    shipCove: "#6D93BE",
+    anakiwa: "#8CC2FF",
+    tropicalBlue: "#CDE0F7",
+    ironTransparent: "rgba(209, 213, 218, 0.3)",
+    white: "#FFFFFF",
+    whiteLilac: "#FBFBFE",
+    porcelain: "#F2F3F4",
+    mercury: "#e5e5e5",
+    slateGray: "#6E7E91",
+    doveGray: "#6D6D6D",
+    tundora: "#414141",
+    mineShaft: "#252525",
+    mineShaftLighter: "#313131",
 };
 
 const common = {
@@ -86,3 +84,4 @@ export const themeDark = {
     },
 
 };
+
