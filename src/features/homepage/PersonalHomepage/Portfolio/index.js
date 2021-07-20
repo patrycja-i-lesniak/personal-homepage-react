99dc4b25c8b8bcad8b7d3ef2ecc849b125369a62
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-20 07:32:30 +0200

    add custom hook to Portfolio

--- src/features/homepage/PersonalHomepage/Portfolio/index.js
+++ src/features/homepage/PersonalHomepage/Portfolio/index.js
@@ -1,7 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { StyledGithubIcon, Header, Section, MyRecentProjects } from "./styled";
 import { SubHeader } from "../SubHeader";
+import { fetchRepositories, selectRepositories, selectRepositoriesStatus } from "../../personalHomepageSlice";
+import { Content } from "./Content";
+import { githubUsername } from "./githubUsername";
+
+
+
 
 export const Portfolio = () => {
+    const dispatch = useDispatch();
+
+    const repositoriesStatus = useSelector(selectRepositoriesStatus);
+    const repositories = useSelector(selectRepositories);
+    
+    useEffect(() => {
+        dispatch(fetchRepositories(githubUsername));
+    }, [dispatch]);
+
     return (
         <Section>
             <Header>
@@ -13,6 +30,10 @@ export const Portfolio = () => {
                     My recent projects
                 </MyRecentProjects>
             </Header>
+            <Content
+            status={repositoriesStatus}
+            repositories={repositories}
+            />
         </Section>
     );
 };
