e0257909229b6d22924e508c36447adf88ed8b2f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-20 05:28:16 +0200

    create slice with three actions, add two selectors

---
+++ src/features/personalHomepageSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const personalHomePageSlice = createSlice({
+    name: "personalHomepage",
+    initialState: {
+        repositories: null,
+        status: "initial",
+    },
+    reducers: {
+        fetchRepositories: () => ({
+            status: "loading",
+            repositories: null,
+        }),
+        fetchRepositoriesSuccess: (_, {payload: repositories}) => ({
+            status: "success",
+            repositories,
+        }),
+        fetchRepositoriesError: () => ({
+            status: "error",
+            repositories: null,
+        }),
+    },
+});
+
+export const {
+    fetchRepositories,
+    fetchRepositoriesSuccess,
+    fetchRepositoriesError,
+} = personalHomePageSlice.actions;
+
+const selectPersonalHomepageState = state => state.personalHomepage;
+
+export const selectRepositories = state => selectPersonalHomepageState(state).repositories;
+export const selectRepositoriesStatus = state => selectPersonalHomepageState(state).status;
+
+export default personalHomePageSlice.reducer;
