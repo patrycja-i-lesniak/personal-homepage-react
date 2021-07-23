9348122c635c0c18059511902d259a853c334080
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-23 09:40:51 +0200

    delete icon from MaiInformation

--- src/features/homepage/PersonalHomepage/MainInformation/index.js
+++ src/features/homepage/PersonalHomepage/MainInformation/index.js
@@ -1,7 +1,6 @@
 import patrycjaLesniakProfile from "./profile.jpg";
-import { Avatar, StyledButtonLink, ButtonIcon, Name, Summary, ThisIs, Wrapper, Icon } from "./styled";
+import { Avatar, StyledButtonLink, ButtonIcon, Name, Summary, ThisIs, Wrapper } from "./styled";
 import { email } from "../email";
-import flamingo from "./flamingo.png";
 
 export const MainInformation = () => (
     <Wrapper>
@@ -10,7 +9,6 @@ export const MainInformation = () => (
             <ThisIs>This is</ThisIs>
             <Name>Patrycja Leśniak</Name>
             <Summary>
-                <Icon src={flamingo} alt="" />
                 I'm.... lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </Summary>
             <StyledButtonLink href={`mailto:${email}`} title={email}>
