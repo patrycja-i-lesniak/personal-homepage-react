05fb86edcb2912f19963e56d2e9bbb98c00095c3
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-23 20:00:20 +0200

    change margin, grid gap and border in List & Tile

--- src/features/homepage/PersonalHomepage/Portfolio/Content/Repositories/styled.js
+++ src/features/homepage/PersonalHomepage/Portfolio/Content/Repositories/styled.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import α from "color-alpha";
 
 export const List = styled.ul`
-    margin-top: 24px;
+    margin-top: 40px;
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     list-style: none;
     padding: 0;
-    grid-gap: 32px;
+    grid-gap: 40px;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
         grid-template-columns: 1fr;
@@ -19,13 +19,13 @@ export const Tile = styled.li`
     padding: 56px;
     margin: 0;
     background: ${({ theme }) => theme.colors.boxBackground};
-    border: 6px solid ${({ theme }) => theme.colors.tile.border};
+    border-top: 4px solid ${({ theme }) => theme.colors.tile.border};
     transition: border-color 0.3s;
     box-shadow: ${({ theme }) => theme.boxShadow};
     border-radius: ${({ theme }) => theme.borderRadiusSmall};
 
     &:hover {
-        border-color: ${({ theme }) => theme.colors.tile.borderHover}
+        border-top: 4px solid ${({ theme }) => theme.colors.tile.borderHover};
     }
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
@@ -37,7 +37,7 @@ export const Name = styled.h3`
     font-size: 24px;
     color: ${({ theme }) => theme.colors.tile.header};
     margin: 0;
-    
+
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         font-size: 16px;
     }
@@ -47,7 +47,8 @@ export const Name = styled.h3`
 export const Description = styled.p`
     margin-top: 24px;
     line-height: 1.4;
-    
+    margin: 0;
+
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         margin-top: 16px;
         font-size: 14px;
