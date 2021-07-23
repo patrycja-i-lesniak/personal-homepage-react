3f18bb1be9a73eb51921771934c1da0921053c8d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-23 10:40:25 +0200

    upload main information

--- src/features/homepage/PersonalHomepage/MainInformation/index.js
+++ src/features/homepage/PersonalHomepage/MainInformation/index.js
@@ -9,7 +9,8 @@ export const MainInformation = () => (
             <ThisIs>This is</ThisIs>
             <Name>Patrycja Leśniak</Name>
             <Summary>
-                I'm.... lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Taking the first step is always hard, but when you see the goal, life becomes more beautiful. 
+            I'm a Graphic Designer, online store owner and a future Frontend Developer. Currently, I'm learning the basics of programming and I hope that my passion will soon change in a way of life.  
             </Summary>
             <StyledButtonLink href={`mailto:${email}`} title={email}>
                 <ButtonIcon />
