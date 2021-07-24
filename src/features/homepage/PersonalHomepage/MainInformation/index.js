35d81b4cb2cb2adfcb42443ced519edeb5fb7d98
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-24 20:28:06 +0200

    change avatar position

--- src/features/homepage/PersonalHomepage/MainInformation/index.js
+++ src/features/homepage/PersonalHomepage/MainInformation/index.js
@@ -4,18 +4,18 @@ import { email } from "../email";
 
 export const MainInformation = () => (
     <Wrapper>
+        <Avatar src={patrycjaLesniakProfile} alt="Patrycja Leśniak" />
         <div>
             <ThisIs>This is</ThisIs>
             <Name>Patrycja Leśniak</Name>
             <Summary>
-            Taking the first step is always hard, but when you see the goal, life becomes more beautiful. 
-            I'm a Graphic Designer, online store owner and a future Frontend Developer. Currently, I'm learning the basics of programming and I hope that my passion will soon change in a way of life.  
+                Taking the first step is always hard, but when you see the goal, life becomes more beautiful.
+                I'm a Graphic Designer, online store owner and a future Frontend Developer. Currently, I'm learning the basics of programming and I hope that my passion will soon change in a way of life.
             </Summary>
             <StyledButtonLink href={`mailto:${email}`} title={email}>
                 <ButtonIcon />
                 Hire me
             </StyledButtonLink>
         </div>
-        <Avatar src={patrycjaLesniakProfile} alt="Patrycja Leśniak" />
     </Wrapper>
 );
